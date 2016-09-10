68234a95e186725718920537f72753379a114c87
Rasmus Antons <mail@rasmusantons.de>
2016-09-10 02:30:17 +0200

    created libgdx project

---
+++ client/desktop/src/net/frozenbit/strategicelements/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.frozenbit.strategicelements.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.frozenbit.strategicelements.StrategicElementsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StrategicElementsGame(), config);
+	}
+}
